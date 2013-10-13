a439ccc9ad0ef9360a1fd1d99c17efd446973609
Hector Guerrero <hector.guerrero.1325@gmail.com>
2013-10-12 21:03:14 -0700

    Correccion de errores
    - se corrigieron errores de compilacion en el jj
    - se acomodaron los valores de los tipos de datos dependiendo de su
    posicion en los tokens
    - se arreglo el codigo de ejemplo

--- TablaDeSimbolos.java
+++ TablaDeSimbolos.java
@@ -16,29 +16,29 @@ public class TablaDeSimbolos extends Object {
 	
 	public static void InicializaListaCompatibilidad() {
 		_enteros.add(44);
-		_enteros.add(48);
+		_enteros.add(51);
 		
 		_dobles.add(44);
 		_dobles.add(45);
-		_dobles.add(48);
-		_dobles.add(50);
+		_dobles.add(51);
+		_dobles.add(53);
 		
 		_caracteres.add(46);
-		_caracteres.add(52);
+		_caracteres.add(55);
 		
 		_cadenas.add(47);
-		_cadenas.add(51);
+		_cadenas.add(54);
 
-		_booleanos.add(54);
-		_booleanos.add(55);
-		_booleanos.add(56);
+		_booleanos.add(48);
+		_booleanos.add(49);
+		_booleanos.add(50);
 	}
 	
 	public static String RevisaAsignacion(Token tokenIzq, Token tokenAsig) {
 		int tipoIdentificador1;
 		int tipoIdentificador2;
 		
-		if(tokenIzq.kind != 48 && tokenIzq.kind != 50) {
+		if(tokenIzq.kind != 51 && tokenIzq.kind != 53) {
 			try {
 				tipoIdentificador1 = (Integer)_tablaTokens.get(tokenIzq.image);
 			}
@@ -50,7 +50,7 @@ public class TablaDeSimbolos extends Object {
 			tipoIdentificador1 = 0;
 		}
 		
-		if(tokenAsig.kind == 49) {
+		if(tokenAsig.kind == 52) {
 			try {
 				tipoIdentificador2 = (Integer)_tablaTokens.get(tokenAsig.image);
 			}
@@ -58,63 +58,66 @@ public class TablaDeSimbolos extends Object {
 				return "Error: El identificador " + tokenAsig.image + " no ha sido declarado\r\nLinea: " + tokenAsig.beginLine;
 			}
 		}
-		else if(tokenAsig.kind == 48 || tokenAsig.kind == 50 || tokenAsig.kind == 51 || tokenAsig.kind == 52 || tokenAsig.kind == 55 || tokenAsig.kind == 56) {
+		else if(tokenAsig.kind == 49 || tokenAsig.kind == 50 || tokenAsig.kind == 51 || tokenAsig.kind == 53 || tokenAsig.kind == 54 || tokenAsig.kind == 55) {
 			tipoIdentificador2 = tokenAsig.kind;
 		}
 		else {
 			tipoIdentificador2 = 0;
 		}
 
+		String mensaje = "";
 		switch(tipoIdentificador1) {
 			case 44:			
 				if( _enteros.contains(tipoIdentificador2) ) {
-					return " ";
+					mensaje = " ";
 				}
 				else {
-					return "Error: No se puede convertir " + tokenAsig.image + " a Entero \r\nLinea: " + tokenIzq.beginLine;
+					mensaje = "Error: No se puede convertir " + tokenAsig.image + " a Entero \r\nLinea: " + tokenIzq.beginLine;
 				}
 				break;
 			case 45:
 				if( _dobles.contains(tipoIdentificador2) ) {
-					return " ";
+					mensaje = " ";
 				}
 				else {
-					return "Error: No se puede convertir " + tokenAsig.image + " a Doble \r\nLinea: " + tokenIzq.beginLine;
+					mensaje = "Error: No se puede convertir " + tokenAsig.image + " a Doble \r\nLinea: " + tokenIzq.beginLine;
 				}
 			case 46:
 				validaChar++;
 				if(validaChar < 2) {
 					if( _caracteres.contains(tipoIdentificador2) ) {
-						return " ";
+						mensaje = " ";
 					}
 					else {
-						return "Error: No se puede convertir " + tokenAsig.image + " a Caracter \r\nLinea: " + tokenIzq.beginLine;
+						mensaje = "Error: No se puede convertir " + tokenAsig.image + " a Caracter \r\nLinea: " + tokenIzq.beginLine;
 					}
 				}
 				else {
-					return "Error: No se puede asignar mas de un valor a un caracter \r\nLinea: " + tokenIzq.beginLine;
+					mensaje = "Error: No se puede asignar mas de un valor a un caracter \r\nLinea: " + tokenIzq.beginLine;
 				}
 				break;
 			case 47:
 				if( _cadenas.contains(tipoIdentificador2) ) {
-					return " ";
+					mensaje = " ";
 				}
 				else {
-					return "Error: No se puede convertir " + tokenAsig.image + " a Cadena \r\nLinea: " + tokenIzq.beginLine;
+					mensaje = "Error: No se puede convertir " + tokenAsig.image + " a Cadena \r\nLinea: " + tokenIzq.beginLine;
 				}
 				break;
-			case 54:
+			case 48:
 				if( _booleanos.contains(tipoIdentificador2) ) {
-					return " ";
+					mensaje = " ";
 				}
 				else {
-					return "Error: No se puede convertir " + tokenAsig.image + " a Booleano \r\nLinea: " + tokenIzq.beginLine;
+					mensaje = "Error: No se puede convertir " + tokenAsig.image + " a Booleano \r\nLinea: " + tokenIzq.beginLine;
 				}
 				break;
 			default:
-				return "El identificador " + tokenIzq.image + " no ha sido declarado \r\nLinea: " + tokenIzq.beginLine;
+				mensaje = "El identificador " + tokenIzq.image + " no ha sido declarado \r\nLinea: " + tokenIzq.beginLine;
 				break;
 		}
+		
+		return mensaje;
 	}
 
 	public static String RevisaVariable(Token token) {
